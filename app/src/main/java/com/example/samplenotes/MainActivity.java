1fc68c1faabea1ca625f8c3ae34be344518207fc
Mohit Singh <singhmohit9718@gmail.com>
2023-02-08 23:01:38 +0530

    Integrate the login skipped part if the user is already logged In
    Signed-off-by: Mohit Singh <singhmohit9718@gmail.com>

--- app/src/main/java/com/example/samplenotes/MainActivity.java
+++ app/src/main/java/com/example/samplenotes/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     String s1[]={"1st Semester","2nd Semester","3rd Semester","4th Semester","5th Semester","6th Semester","7th Semester","8th Semester"};
     int image[]={R.drawable.sem1,R.drawable.sem2,R.drawable.sem3,R.drawable.sem4,R.drawable.sem5,R.drawable.sem6,R.drawable.sem7,R.drawable.sem8};
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(myAdapter);
         ActionBar actionBar=getSupportActionBar();
         //actionBar.hide();
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+
+        if (mFirebaseUser != null){
+
+        }else{
+            startActivity(new Intent(MainActivity.this,OTPscreen.class));
+            finish();
+        }
     }
 }
